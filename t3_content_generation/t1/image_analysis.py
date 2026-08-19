5c88b9d48bb2d0635614c8bea3642a84385eef4c
Roland Lavati <roland_lavati@epam.com>
2026-08-19 10:58:26 +0200

    t3/t1

--- t3_content_generation/t1/image_analysis.py
+++ t3_content_generation/t1/image_analysis.py
@@ -1,13 +1,13 @@
 import base64
+from pathlib import Path
 
-from commons.constants import OPENAI_HOST
+from commons.constants import OPENAI_CHAT_COMPLETIONS_ENDPOINT
 from t3_content_generation._openai_client import OpenAIClientT3
-
 
 # https://developers.openai.com/api/docs/guides/images-vision?format=url&lang=curl
 # https://developers.openai.com/api/docs/guides/images-vision?format=base64-encoded
 
-#TODO:
+# TODO:
 # You need to analyse these 2 images:
 #   - https://a-z-animals.com/media/2019/11/Elephant-male-1024x535.jpg
 #   - in this folder we have 'logo.png', load it as encoded data (see documentation)
@@ -18,3 +18,32 @@ from t3_content_generation._openai_client import OpenAIClientT3
 #   - Function to encode image to base64 you can find in documentation
 # ---
 # In the end load both images (url and base64 encoded 'logo.png'), ask "Generate poem based on images" and se what will happen?
+
+client = OpenAIClientT3(OPENAI_CHAT_COMPLETIONS_ENDPOINT)
+
+logo_path = Path(__file__).parent / "logo.png"
+encoded_logo = base64.b64encode(logo_path.read_bytes()).decode("utf-8")
+
+client.call(
+    print_request=True,
+    print_response=True,
+    model="gpt-5.2",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "Generate poem based on images"},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": "https://a-z-animals.com/media/2019/11/Elephant-male-1024x535.jpg"
+                    },
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {"url": f"data:image/png;base64,{encoded_logo}"},
+                },
+            ],
+        }
+    ],
+)
